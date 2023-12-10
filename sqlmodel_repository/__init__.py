3b2275121de2feec42878be17a5d44ca38041716
Dmitrij Vinokour <dmitrij.vinokour@cojodi.com>
2023-12-10 13:28:57 +0100

    feat: join_where

--- sqlmodel_repository/__init__.py
+++ sqlmodel_repository/__init__.py
@@ -33,36 +33,45 @@ class Repository(Generic[T]):
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
     ):  # pragma: no cover
-        q = self._build_query(where, relationships, order_by)
+        q = self._build_query(where, join_where, relationships, order_by)
         return self.db.exec(q).first()
 
     def one(
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
     ):  # pragma: no cover
-        q = self._build_query(where, relationships, order_by)
+        q = self._build_query(where, join_where, relationships, order_by)
         return self.db.exec(q).one()
 
     def all(
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
         offset: int = 0,
         limit: int = 0,
     ):  # pragma: no cover
-        q = self._build_query(where, relationships, order_by, offset, limit)
+        q = self._build_query(where, join_where, relationships, order_by, offset, limit)
         return self.db.exec(q).all()
 
     def _build_query(
-        self, where, relationships=None, order_by=None, offset: int = 0, limit: int = 0
+        self,
+        where,
+        join_where=None,
+        relationships=None,
+        order_by=None,
+        offset: int = 0,
+        limit: int = 0,
     ):
         q = select(self.table_cls)
         if limit > 0:
@@ -74,6 +83,12 @@ class Repository(Generic[T]):
 
         if where is not None:
             q = q.where(where)
+
+        if join_where is not None:
+            for join_where_ in join_where:
+                join_table, join_condition = join_where_
+                q = q.join(join_table)
+                q = q.where(join_condition)
 
         if order_by is not None:
             for order_by_ in order_by:
@@ -90,32 +105,35 @@ class AsyncRepository(Repository[T]):
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
     ):
-        q = self._build_query(where, relationships, order_by)
+        q = self._build_query(where, join_where, relationships, order_by)
         return (await self.db.exec(q)).first()
 
     async def one(
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
     ):
-        q = self._build_query(where, relationships, order_by)
+        q = self._build_query(where, join_where, relationships, order_by)
         return (await self.db.exec(q)).one()
 
     async def all(
         self,
         *,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
         offset: int = 0,
         limit: int = 0,
     ):
-        q = self._build_query(where, relationships, order_by, offset, limit)
+        q = self._build_query(where, join_where, relationships, order_by, offset, limit)
         return (await self.db.exec(q)).all()
 
     async def first_or_create(
@@ -123,12 +141,16 @@ class AsyncRepository(Repository[T]):
         *,
         orm: SQLModel | None = None,
         where: bool | None = None,
+        join_where: list[tuple[SQLModel, bool]] | None = None,
         relationships: list[Any] | None = None,
         order_by: list[Any] | None = None,
         **kwargs: T,
     ):
         o = await self.first(
-            where=where, relationships=relationships, order_by=order_by
+            where=where,
+            join_where=join_where,
+            relationships=relationships,
+            order_by=order_by,
         )
         if o is None:
             o = self.create(orm=orm, **kwargs)
